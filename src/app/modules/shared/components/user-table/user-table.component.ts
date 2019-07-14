57a5d1188671e0360db823a16a4604692900d602
renzoventura <edrickventura1996@gmail.com>
2019-07-14 18:47:57 +1200

    57 - facebook type assigning and created new application page additions

--- src/app/modules/shared/components/user-table/user-table.component.ts
+++ src/app/modules/shared/components/user-table/user-table.component.ts
@@ -17,6 +17,7 @@ export class UserTableComponent implements OnInit {
   suburb = ""
 
   suburbs = ["Albany", "Bayswater", "Bayview", "Beach Haven", "Belmont", "Birkdale", "Birkenhead", "Browns Bay", "Campbells Bay", "Castor Bay", "Chatswood", "Cheltenham", "Crown Hill", "Devonport", "Fairview Heights", "Forrest Hill", "Glenfield", "Greenhithe", "Hauraki", "Highbury", "Long Bay", "Mairangi Bay", "Pinehill", "Rosedale", "Rothesay Bay", "Sunnynook", "Takapuna", "Torbay", "Wairau Valley", "Westlake"];
+  
   constructor(private authServer: AuthService) { }
 
   ngOnInit() {
